d4dfc2df0d921aeab4fd13805feb44c65f8b383f
Sergei Kharitontcev-Beglov <khbminus@gmail.com>
2023-07-26 09:19:51 +0200

    Added chevron icon

--- app/components/source-map/TabView.tsx
+++ app/components/source-map/TabView.tsx
@@ -1,4 +1,5 @@
 import {Listbox} from '@headlessui/react'
+import {ChevronUpDownIcon} from "@heroicons/react/20/solid";
 
 interface TabViewProps {
     names: string[],
@@ -8,10 +9,13 @@ interface TabViewProps {
 
 export default function TabView({names, setSelectedName, selectedName}: TabViewProps) {
     return (
-        <Listbox value={selectedName} onChange={setSelectedName} >
+        <Listbox value={selectedName} onChange={setSelectedName}>
             <Listbox.Button
                 className="h-9 relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
                 <span className="truncate">{names[selectedName]}</span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+                    <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+                </span>
             </Listbox.Button>
             <Listbox.Options
                 className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
